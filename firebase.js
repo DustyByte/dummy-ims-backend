6211025174896dec40567b7d08b6a92249a76652
Mashnun ur Rahman <164175863+DustyByte@users.noreply.github.com>
2024-09-01 20:57:47 +0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+const firebaseConfig = {
+  apiKey: "AIzaSyDAbhas-bEu5sg22qQ3zt7LtINlfsInnuY",
+  authDomain: "dummyims.firebaseapp.com",
+  projectId: "dummyims",
+  storageBucket: "dummyims.appspot.com",
+  messagingSenderId: "196039860313",
+  appId: "1:196039860313:web:f71434c58edd2ca6671148"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
