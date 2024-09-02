4eb86744e33ce0b29d234b9d078f6f88f8e0c7e7
Mashnun ur Rahman <164175863+DustyByte@users.noreply.github.com>
2024-09-02 19:00:31 +0600

    Updated server.js

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ const port = process.env.port || 3000
 
 app.use(express.json())
 
-app.use(cors({
-    origin: `http:/localhost:3000`
-}))
+app.use(cors())
 
 app.post('/', async (req, res) => {
     const request = { ...req.body }
